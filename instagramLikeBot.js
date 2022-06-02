8dd3358e9c78bc575301af5a3807cf381768cea0
Caleb <87406555+caleblopez96@users.noreply.github.com>
2022-06-01 20:09:56 -0500

    Add files via upload

--- instagramLikeBot.js
+++ instagramLikeBot.js
@@ -4,16 +4,16 @@
 // define next picture button
 // if theres a heart, click heart, add 1 to likes and log to console the string
 // click the parent element of arrow to go to new photo
-// 
+
 
 let likes = 0;
 setInterval(() => {
     const likeButton = document.querySelector('svg[aria-label="like"[width ="24"]');
-    const nextPictureButton = document.querySelector('svg [aria-label="Next"]');
-    if (heart) {
-        heart.parentNode.parentElement.click()
+    const nextPostButton = document.querySelector('svg [aria-label="Next"]');
+    if (likeButton) {
+        likeButton.parentNode.parentElement.click()
         likes++;
         console.log(`You've liked ${likes} post(s)`);
     }
-    arrow.parentElement.parentElement.click();
+    nextPostButton.parentElement.parentElement.click();
 }, 20000);
