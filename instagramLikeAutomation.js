16b1e22b4ba84a33ed780643acbd1f9b09d3adcd
Caleb <87406555+caleblopez96@users.noreply.github.com>
2022-06-23 04:09:17 -0500

    Add files via upload
    Added more variations to help avoid bans when afk

--- instagramLikeAutomation.js
+++ instagramLikeAutomation.js
@@ -1,12 +1,91 @@
-// initialize likes to 0
-// set interval and seconds (in ms)
-// define like button
-// define next picture button
-// if theres a likeButton, click likeButton,
-// add 1 to likes
-// log to console the string
-// click the arrow to go to new photo
+//* Option One - like limit and alert (best option to avoid ban):
 
+// initialize variable that keeps track of how many likes and set to 0.
+// define function to alert user of max likes.
+// set interval.
+// define instagram's like button.
+// define instagrams next button.
+// if likes is equal to 700.
+// clear interval & return limit function.
+// else if likes is less than or equal to 700.
+// if theres a like button, click it. 
+// add 1 to likes.
+// log to console the string.
+// clicks the arrow to go to next photo. 
+
+
+let likes = 0;
+
+let limit = () => {
+    return alert(`You've liked ${likes} post(s). ${likes} is the maximum amount of likes per day.`);
+};
+
+let automator = setInterval(() => {
+    const likeButton = document.querySelector('svg[aria-label="Like"]');
+    const nextButton = document.querySelector('svg[aria-label="Next"]');
+    if (likes === 700) {
+        clearInterval(automator);
+        return limit();
+    } 
+    else if (likes <= 700) {
+            if (likeButton.getAttribute("aria-label") === "Like") {
+            likeButton.parentNode.parentElement.click();
+            likes++;
+            console.log(`You've liked ${likes} post(s)`); 
+        };
+            nextButton.parentElement.parentNode.click();
+    }}, 20000);
+
+
+
+
+
+//* option 2 - like limit, but no alert
+
+// initialize variable that keeps track of how many likes and set to 0.
+// set interval.
+// define instagram's like button.
+// define instagrams next button.
+// while likes is less than or equal to 700.
+// if theres a like button, click it. 
+// add 1 to likes.
+// log to console the string.
+// click the arrow to go to new photo.
+
+/*
+
+let likes = 0;
+
+setInterval(() => {
+    const likeButton = document.querySelector('svg[aria-label="Like"]');
+    const nextButton = document.querySelector('svg[aria-label="Next"]');
+    while (likes <= 700) {
+    if (likeButton.getAttribute("aria-label") === "Like") {
+        likeButton.parentNode.parentElement.click();
+        likes++;
+        console.log(`You've liked ${likes} post(s)`); 
+    }
+        nextButton.parentElement.parentNode.click();
+}}, 20000); 
+
+*/ 
+
+
+
+
+
+//* option 3 - no like limit and no alert:
+
+// initialize variable that keeps track of how many likes and set to 0.
+// set interval.
+// define instagram's like button.
+// define instagrams next button.
+// if theres a like button, click it. 
+// add 1 to likes.
+// log to console the string.
+// click the arrow to go to new photo.
+
+/*
 
 let likes = 0;
 
@@ -20,6 +99,12 @@ setInterval(() => {
     }
         nextButton.parentElement.parentNode.click();
 }, 20000);
+
+*/
+
+
+
+
 
 
 
