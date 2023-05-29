50ce506e47ae38a58ccd620dc2e0e5b55d81d370
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-05-28 21:23:16 -0700

    Add files via upload

--- automationScript.js
+++ automationScript.js
@@ -1,17 +1,18 @@
-// Intiliaze a variable to keep count of the amount of likes.
+// Intiliaze variables to keep count of the amount of likes.
 let likes = 0;
 const MAX_LIKES = 700;
+let message = `You've liked ${likes} post(s). ${likes} is the maximum amount of likes per day.`;
 
 // function to alert user they have hit the limit.
 let limit = () => {
-    return alert(`You've liked ${likes} post(s). ${likes} is the maximum amount of likes per day.`);
+    return alert(message);
 };
 
 // the interval sets the time between likes.
 const automator = (interval)=>{
     setInterval(() => {
 
-    // storing necessary DOM elements into variables
+    // storing necessary DOM elements into variables.
     const likeButton = document.querySelector('svg[aria-label="Like"]');
     const nextButton = document.querySelector('svg[aria-label="Next"]');
 
