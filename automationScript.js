fd490790108a7c9f20a513c927cdbc4334dccfda
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-06-01 13:19:07 -0700

    Add files via upload

--- automationScript.js
+++ automationScript.js
@@ -4,9 +4,7 @@ const MAX_LIKES = 700;
 let message = `You've liked ${likes} post(s). ${likes} is the maximum amount of likes per day.`;
 
 // Function to alert user they have hit the limit.
-let limit = () => {
-    return alert(message);
-};
+let limit = () => alert(message);
 
 // The interval parameter sets the time between likes
 const automator = (interval) => {
@@ -20,16 +18,16 @@ const automator = (interval) => {
         return limit();
     }
     else if (likes <= MAX_LIKES) {
-            // If likeButton is present on page
-            if (likeButton.getAttribute("aria-label") === "Like") {
-            // Click like button
-            likeButton.parentNode.parentElement.click();
-            // Increase the count of likes
-            likes++;
-            console.log(`You've liked ${likes} post(s)`); 
+        // If likeButton is present on page
+        if (likeButton.getAttribute("aria-label") === "Like") {
+        // Click like button
+        likeButton.parentNode.parentElement.click();
+        // Increase the count of likes
+        likes++;
+        console.log(`You've liked ${likes} post(s)`); 
         };
-            // Click next button
-            nextButton.parentElement.parentNode.click();
+        // Click next button
+        nextButton.parentElement.parentNode.click();
     }
     // Time in milliseconds
 }, interval);
@@ -37,3 +35,23 @@ const automator = (interval) => {
 
 // calling the function with a 20 second interval
 automator(20000);
+
+
+
+/* 
+
+https://www.instagram.com/explore/tags/nameOfHashTagGoesHere/
+
+*/
+
+// create a chrome extension that list the steps
+// has a button
+// button when clicked runs automator(20000);
+
+
+// step 1: Log into instagram.
+// step 2: Search for a hashtag.
+// step 3: Navigate to the "Most recent post" section.
+// step 4: Click on the any photo.
+// step 5: Click Automate!
+
